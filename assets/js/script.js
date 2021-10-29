ce4b3423bd764088d3a7dc16724ffa9a2d97a389
kngurley <kngurley25@gmail.com>
2021-10-29 15:52:24 -0500

    added code for local storage of name and ship

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,34 +101,73 @@ var formSubmitHandler = function(event) {
 
     var inputName = inputNameEl.value.trim();
 
+    var starShip = shipDropdownEl.options[shipDropdownEl.selectedIndex].text;
+
+    var combined = inputName + "," + starShip;
+
     if (inputName) {
-        displayData(inputName);
+        displayData(inputName, starShip);
+
+        inputNameEl.value = "";
     }
+
+    // save submitted passengers to array
+    if (pastSubmits.includes(inputName) === false) {
+        pastSubmits.push(combined);
+    }
+
+    saveSubmits();
 }
 
 // function to display name and starship data to page
-var displayData = function(inputName, shipName) {
+var displayData = function(inputName, starShip) {
+
+    nameAndStarshipEl.textContent = "";
 
     var displayName = document.createElement("p");
     displayName.textContent = "Passenger Name: " + inputName;
 
     var displayShipName = document.createElement("p");
-    displayShipName.textContent = "Selected Starship: " + shipDropdownEl.options[shipDropdownEl.selectedIndex].text;
+    displayShipName.textContent = "Selected Starship: " + starShip;
 
     var launch = document.createElement("p");
-    launch.textContent = "Scheduled Launch: " + moment().add(7, 'days').format("dddd, MMMM Do YYYY, h:mm:ss a");
-    
-    var message = document.createElement("p");
-    message.textContent = "Good luck!";
+    launch.textContent = "Scheduled Launch: " + moment().add(3, 'days').format("dddd, MMMM Do YYYY, h:mm:ss a");
 
     nameAndStarshipEl.appendChild(displayName);
     nameAndStarshipEl.appendChild(displayShipName);
     nameAndStarshipEl.appendChild(launch);
-    nameAndStarshipEl.appendChild(message);
+}
+
+// local storage
+var pastSubmits = [];
+var saveSubmits = function() {
+    localStorage.setItem("passengers", JSON.stringify(pastSubmits));
+}
+
+var loadSubmits = function() {
+
+    var savedPassengers = JSON.parse(localStorage.getItem("passengers"));
+    console.log(savedPassengers);
+
+    if (!savedPassengers) {
+        return false;
+    }
+
+    for (var i = 0; i < savedPassengers.length; i++) {
+
+        var pastTravelers = document.createElement("p");
+        pastTravelers.textContent = "Past Traveler: " + savedPassengers[i].split(",")[0] + " - " + savedPassengers[i].split(",")[1];
+        nameAndStarshipEl.appendChild(pastTravelers);
+
+        pastSubmits.push(pastSubmits[i]);
+    }
+
+    localStorage.setItem("passengers", JSON.stringify(savedPassengers));
 }
 
 // event listener to starship buttons
 shipButtonEl.addEventListener("click", verifyStarship);
 submitButtonEl.addEventListener("click", formSubmitHandler);
 
+loadSubmits();
 
