621f63b4d04067f12cd673c59617a12d62fe9283
Jeramy Sena <jeramy_sena@hotmail.com>
2021-11-02 19:17:46 -0500

    fixed html and updated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,41 @@
-
 //HTML elements
 var whoInSpaceEl = document.querySelector("#who-in-space-button");
-var peopleSelectionEl = document.querySelector("#who-space-results");
+var peopleSelectionEl = document.querySelector("#person-in-space");
+var craftSelectionEl = document.querySelector("#craft-in-space");
+var whoElseSpaceEl = document.querySelector("#who-else-in-space-button");
+
+// setting up array for displayNames
+var peopleInSpace;
+var peopleInSpaceIndex = 0;
 
 // open notify API source people in space right now
 var getSpacePeople = function () {
+  var apiUrl = "http://api.open-notify.org/astros.json";
 
-    var apiUrl = "http://api.open-notify.org/astros.json";
+  fetch(apiUrl).then(function (response) {
+    console.log(response);
+    console.log(apiUrl.value);
+    if (response.ok) {
+      response.json().then(function (peopleInfo) {
+        console.log(peopleInfo);
+        peopleInSpace = peopleInfo.people;
+        console.log(peopleInSpace);
+        displayNames();
+      });
+    }
+  });
+};
 
-    fetch(apiUrl)
-        .then(function(response) {
-            console.log(response);
-            console.log(apiUrl.value);
-            if (response.ok) {
-                response.json().then(function(peopleInfo) {
-                    console.log(peopleInfo);
-                   
-                    displayNames(peopleInfo);
-
-                })
-            }
-        })
+// display number of people in space
+function displayNames() {
+  peopleSelectionEl.textContent = peopleInSpace[peopleInSpaceIndex].name;
+  craftSelectionEl.textContent = peopleInSpace[peopleInSpaceIndex].craft;
+  peopleInSpaceIndex++;
 }
 
-var displayNames = function(peopleData) {
-    
-    // display number of people in space
-    var number = document.createElement("p");
-    number.textContent = peopleData.number;
-    
-    // would loop through to display all names and craft to page
-    var names = document.createElement("p");
-    names.textContent = peopleData.people[0].name;
-
-    peopleSectionEl.appendChild(number);
-    peopleSectionEl.appendChild(names);
-
-}
-
+// event listeners for people in space
 whoInSpaceEl.addEventListener("click", getSpacePeople);
-
+whoElseSpaceEl.addEventListener("click", getSpacePeople);
 
 //HTML elements
 var starshipEl = document.querySelector("#starship-info");
@@ -55,186 +51,181 @@ var tableHeadingEl = document.querySelector("#table");
 
 // ship array
 var shipObject = {
-    3: "star-destroyer", // starships/3/
-    9: "death-star", // starships/9/
-    10: "millennium-falcon", // starships/10/
-    11: "y-wing", // starships/11/
-    12: "x-wing", // starships/12/
-    17: "rebel-transport" // starships/17/
-}
+  3: "star-destroyer", // starships/3/
+  9: "death-star", // starships/9/
+  10: "millennium-falcon", // starships/10/
+  11: "y-wing", // starships/11/
+  12: "x-wing", // starships/12/
+  17: "rebel-transport", // starships/17/
+};
 
 var verifyStarship = function (event) {
-    
-    var shipSelect = event.target.getAttribute("id");
+  var shipSelect = event.target.getAttribute("id");
 
-    if (shipSelect === shipObject[3]) {
-        var shipNum = 3;
-    } else if (shipSelect === shipObject[9]) {
-        var shipNum = 9;
-    } else if (shipSelect === shipObject[10]) {
-        var shipNum = 10;
-    } else if (shipSelect === shipObject[11]) {
-        var shipNum = 11;
-    } else if (shipSelect === shipObject[12]) {
-        var shipNum = 12;
-    } else if (shipSelect === shipObject[17]) {
-        var shipNum = 17;
-    }
+  if (shipSelect === shipObject[3]) {
+    var shipNum = 3;
+  } else if (shipSelect === shipObject[9]) {
+    var shipNum = 9;
+  } else if (shipSelect === shipObject[10]) {
+    var shipNum = 10;
+  } else if (shipSelect === shipObject[11]) {
+    var shipNum = 11;
+  } else if (shipSelect === shipObject[12]) {
+    var shipNum = 12;
+  } else if (shipSelect === shipObject[17]) {
+    var shipNum = 17;
+  }
 
-    getStarship(shipNum);
-}
+  getStarship(shipNum);
+};
 
 // Star Wars API call
 var getStarship = function (shipNum) {
+  var apiUrl = "https://swapi.dev/api/starships/" + shipNum + "/";
 
-    var apiUrl = "https://swapi.dev/api/starships/" + shipNum + "/";
+  fetch(apiUrl).then(function (response) {
+    // console.log(response);
+    if (response.ok) {
+      response.json().then(function (starship) {
+        console.log(starship);
 
-    fetch(apiUrl)
-        .then(function(response) {
-            // console.log(response);
-            if (response.ok) {
-                response.json().then(function(starship) {
-                    console.log(starship);
-
-                    starshipInfo(starship);
-
-                })
-            }
-        })
-}
+        starshipInfo(starship);
+      });
+    }
+  });
+};
 
 // display starship info to page
-var starshipInfo = function(shipData) {
+var starshipInfo = function (shipData) {
+  // clear old content
+  starshipEl.textContent = "";
 
-    // clear old content
-    starshipEl.textContent = "";
+  // starship data to display
 
-    // starship data to display
-    
-    var shipName = document.createElement("p");
-    shipName.textContent = shipData.name;
-    shipName.classList = "card-header-title";
+  var shipName = document.createElement("p");
+  shipName.textContent = shipData.name;
+  shipName.classList = "card-header-title";
 
-    var shipModel = document.createElement("p");
-    shipModel.textContent = "Model: " + shipData.model;
+  var shipModel = document.createElement("p");
+  shipModel.textContent = "Model: " + shipData.model;
 
-    var shipManufacturer = document.createElement("p");
-    shipManufacturer.textContent = "Manufacturer: " + shipData.manufacturer;
+  var shipManufacturer = document.createElement("p");
+  shipManufacturer.textContent = "Manufacturer: " + shipData.manufacturer;
 
-    var shipHyperdrive = document.createElement("p");
-    shipHyperdrive.textContent = "Hyperdrive Rating: " + shipData.hyperdrive_rating;
+  var shipHyperdrive = document.createElement("p");
+  shipHyperdrive.textContent =
+    "Hyperdrive Rating: " + shipData.hyperdrive_rating;
 
-    var shipCrew = document.createElement("p");
-    shipCrew.textContent = "Crew: " + shipData.crew;
+  var shipCrew = document.createElement("p");
+  shipCrew.textContent = "Crew: " + shipData.crew;
 
-    var shipLength = document.createElement("p");
-    shipLength.textContent = "Length: " + shipData.length + " meters";
+  var shipLength = document.createElement("p");
+  shipLength.textContent = "Length: " + shipData.length + " meters";
 
-    var shipCargo = document.createElement("p");
-    shipCargo.textContent = "Cargo capacity: " + shipData.cargo_capacity + " kilograms";
+  var shipCargo = document.createElement("p");
+  shipCargo.textContent =
+    "Cargo capacity: " + shipData.cargo_capacity + " kilograms";
 
-    starshipEl.appendChild(shipName);
-    starshipEl.appendChild(shipModel);
-    starshipEl.appendChild(shipManufacturer);
-    starshipEl.appendChild(shipHyperdrive);
-    starshipEl.appendChild(shipCrew);
-    starshipEl.appendChild(shipLength);
-    starshipEl.appendChild(shipCargo);
-}
+  starshipEl.appendChild(shipName);
+  starshipEl.appendChild(shipModel);
+  starshipEl.appendChild(shipManufacturer);
+  starshipEl.appendChild(shipHyperdrive);
+  starshipEl.appendChild(shipCrew);
+  starshipEl.appendChild(shipLength);
+  starshipEl.appendChild(shipCargo);
+};
 
 // function to capture name input form
-var formSubmitHandler = function(event) {
-    event.preventDefault();
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-    var inputName = inputNameEl.value.trim();
+  var inputName = inputNameEl.value.trim();
 
-    var starShip = shipDropdownEl.options[shipDropdownEl.selectedIndex].text;
+  var starShip = shipDropdownEl.options[shipDropdownEl.selectedIndex].text;
 
-    var combined = inputName + "," + starShip;
+  var combined = inputName + "," + starShip;
 
-    if (inputName) {
-        displayData(inputName, starShip);
+  if (inputName) {
+    displayData(inputName, starShip);
 
-        inputNameEl.value = "";
-    }
+    inputNameEl.value = "";
+  }
 
-    // save submitted passengers to array
-    if (pastSubmits.includes(inputName) === false) {
-        pastSubmits.push(combined);
-    }
+  // save submitted passengers to array
+  if (pastSubmits.includes(inputName) === false) {
+    pastSubmits.push(combined);
+  }
 
-    saveSubmits();
-}
+  saveSubmits();
+};
 
 // function to display name and starship data to page
-var displayData = function(inputName, starShip) {
+var displayData = function (inputName, starShip) {
+  // nameAndStarshipEl.textContent = "";
 
-    // nameAndStarshipEl.textContent = "";
+  var displayName = document.createElement("p");
+  displayName.textContent = "Passenger Name: " + inputName;
+  displayName.classList = "has-text-weight-bold is-capitalized";
 
-    var displayName = document.createElement("p");
-    displayName.textContent = "Passenger Name: " + inputName;
-    displayName.classList = "has-text-weight-bold is-capitalized";
+  var displayShipName = document.createElement("p");
+  displayShipName.textContent = "Selected Starship: " + starShip;
 
-    var displayShipName = document.createElement("p");
-    displayShipName.textContent = "Selected Starship: " + starShip;
+  var launch = document.createElement("p");
+  launch.textContent =
+    "Scheduled Launch: " +
+    moment().add(3, "days").format("dddd, MMMM Do YYYY, h:mm:ss a");
 
-    var launch = document.createElement("p");
-    launch.textContent = "Scheduled Launch: " + moment().add(3, 'days').format("dddd, MMMM Do YYYY, h:mm:ss a");
+  var message = document.createElement("p");
+  message.textContent = "Good luck space traveler - may the force be with you!";
 
-    var message = document.createElement("p");
-    message.textContent = "Good luck space traveler - may the force be with you!"
-
-    nameAndStarshipEl.prepend(message);
-    nameAndStarshipEl.prepend(launch);
-    nameAndStarshipEl.prepend(displayShipName);
-    nameAndStarshipEl.prepend(displayName);
-}
+  nameAndStarshipEl.prepend(message);
+  nameAndStarshipEl.prepend(launch);
+  nameAndStarshipEl.prepend(displayShipName);
+  nameAndStarshipEl.prepend(displayName);
+};
 
 // local storage
 var pastSubmits = [];
-var saveSubmits = function() {
-    localStorage.setItem("passengers", JSON.stringify(pastSubmits));
-}
+var saveSubmits = function () {
+  localStorage.setItem("passengers", JSON.stringify(pastSubmits));
+};
 
-var loadSubmits = function() {
+var loadSubmits = function () {
+  var savedPassengers = JSON.parse(localStorage.getItem("passengers"));
+  console.log(savedPassengers);
 
-    var savedPassengers = JSON.parse(localStorage.getItem("passengers"));
-    console.log(savedPassengers);
+  if (!savedPassengers) {
+    return false;
+  }
 
-    if (!savedPassengers) {
-        return false;
-    }
+  var pastTravelerHeading = document.createElement("th");
+  pastTravelerHeading.textContent = "Past travelers and starships:";
+  pastPassengerEl.appendChild(pastTravelerHeading);
 
-    var pastTravelerHeading = document.createElement("th");
-    pastTravelerHeading.textContent = "Past travelers and starships:";
-    pastPassengerEl.appendChild(pastTravelerHeading);
+  for (var i = 0; i < savedPassengers.length; i++) {
+    var pastTravelers = document.createElement("tr");
 
-    for (var i = 0; i < savedPassengers.length; i++) {
+    var traveler = document.createElement("td");
+    traveler.textContent = savedPassengers[i].split(",")[0];
+    traveler.classList = "is-capitalized";
 
-        var pastTravelers = document.createElement("tr");
+    var travelerShip = document.createElement("td");
+    travelerShip.textContent = savedPassengers[i].split(",")[1];
 
-        var traveler = document.createElement("td");
-        traveler.textContent = savedPassengers[i].split(",")[0];
-        traveler.classList = "is-capitalized";
+    // pastTravelers.textContent = "Past Traveler: " + savedPassengers[i].split(",")[0] + " - " + savedPassengers[i].split(",")[1];
 
-        var travelerShip = document.createElement("td");
-        travelerShip.textContent = savedPassengers[i].split(",")[1];
-        
-        // pastTravelers.textContent = "Past Traveler: " + savedPassengers[i].split(",")[0] + " - " + savedPassengers[i].split(",")[1];
-        
-        pastTravelers.appendChild(traveler);
-        pastTravelers.appendChild(travelerShip);
-        pastPassengerEl.appendChild(pastTravelers);
+    pastTravelers.appendChild(traveler);
+    pastTravelers.appendChild(travelerShip);
+    pastPassengerEl.appendChild(pastTravelers);
 
-        pastSubmits.push(savedPassengers[i]);
-    }
-    
-    localStorage.setItem("passengers", JSON.stringify(savedPassengers));
-}
+    pastSubmits.push(savedPassengers[i]);
+  }
+
+  localStorage.setItem("passengers", JSON.stringify(savedPassengers));
+};
 
 // event listener to starship buttons
 shipButtonEl.addEventListener("click", verifyStarship);
 submitButtonEl.addEventListener("click", formSubmitHandler);
 
 loadSubmits();
-
